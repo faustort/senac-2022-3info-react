85c66be962062141baf9386d393efcc7a5e19f5b
Your Name <you@example.com>
2022-10-25 15:25:58 -0300

    navegacao

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -4,6 +4,8 @@ import { Home } from "../screens/Home";
 import { Login } from "../screens/Login";
 import { Register } from "../screens/Register";
 import { Sobre } from "../screens/Sobre";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 // // Simboliza a instância do Stack
 // const Stack = createNativeStackNavigator();
@@ -16,26 +18,47 @@ export const RootNavigation = () => {
     return (
         <MBNav.Navigator
             activeColor="#fff"
-            inactiveColor="tomato"
-
+            inactiveColor="rgba(255,255,255,0.5)"
         >
             <MBNav.Screen
                 name="Home"
                 component={Home}
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="home" color={color} size={26} />
+                    ),
+                }}
             />
             <MBNav.Screen
                 name="Sobre"
                 component={Sobre}
-
+                options={{
+                    tabBarLabel: 'Sobre',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="account" color={color} size={26} />
+                    ),
+                }}
             />
             <MBNav.Screen
                 name="Contato"
                 component={Sobre}
-
+                options={{
+                    tabBarLabel: 'Contato',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="phone" color={color} size={26} />
+                    ),
+                }}
             />
             <MBNav.Screen
                 name="Registro"
                 component={Register}
+                options={{
+                    tabBarLabel: 'Registro',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="box-minus-outline" color={color} size={26} />
+                    ),
+                }}
             />
             <MBNav.Screen
                 name="Login"
