3ef16155b289c906f6f31fe5682c6ac0a3a450a4
Your Name <you@example.com>
2022-10-26 15:50:13 -0300

    Login

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,13 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { View } from "react-native";
+import { TextInput } from "react-native-paper";
 import { styles } from "../lib/style";
 export const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   return (
     <View style={styles.container}>
-      <Text style={styles.h1}>👼Aqui é o Login</Text>
-      <Text style={styles.h1}>👼Aqui é o Login</Text>
-      <Text style={styles.h1}>👼Aqui é o Login</Text>
-      <Text style={styles.h1}>👼Aqui é o Login</Text>
-      <Text style={{ ...styles.h1, ...styles.red }}>👼Aqui é o Login</Text>
+      <TextInput
+        label="Email"
+        mode="outlined"
+        value={email}
+        onChangeText={(emailDigitado) => setEmail(emailDigitado)}
+      />
+      <TextInput
+        label="Senha"
+        mode="outlined"
+        value={password}
+        onChangeText={(valor) => setPassword(valor)}
+        secureTextEntry={true}
+      />
     </View>
   );
 };
